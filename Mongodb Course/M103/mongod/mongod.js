d31b4d7f5b1fadddee5cb5c64c0b3b207479baaf
Suraj Karki <suraj.karki500@gmail.com>
2020-08-08 21:30:58 +0545

    Finished server tools

--- Mongodb Course/M103/mongod/mongod.js
+++ Mongodb Course/M103/mongod/mongod.js
@@ -107,5 +107,126 @@ $ mongo admin -u username -p password
 
 <<<<<<<<<<<<<<<<<<<< Basic Security >>>>>>>>>>>>>>>>>>>>>>
 
+Authentication: verifies the identity of a user.
+Answer the question: Who are you ?
+
+Authorization: Verifies the previleges of a user.
+Answer the question: What do you have access to ?
+
+Authentication Mechanisms:(client)
+
+1) SCRAM: It is default authentication mechanism in mongodb. It is password mechanism.
+2) X.509: Uses X.509 certificate for the authentication. It is more secure and complex mechanism.
+
+
+Cluster Authentication Mechanisms:
+It is a intra cluster authentication, the mechanism by which nodes in a cluster authenticate.
+
+Authorization: Role Based Access Control
+-> Each users has one or more role.
+-> Each role has one or more previleges.
+-> A previlege represents a group of Actions and the Resources  those actions to apply.
+
+Roles supports a high level of responsibility isolation for operational tasks.
+Example:
+
+1) Database Administrator: 
+   roles: i) Create User
+         ii) Create Index
+
+2) Developer:
+   roles: i) Write Data
+          ii) Read Data
+
+3) Data Scientist:
+   roles: i) Read Data
+
+Create new user with the root role (also, named root):
+use admin
+db.createUser({
+  user: "root",
+  pwd: "root123",
+  roles : [ "root" ]
+})
+
+MongoDB built roles are as follows:
+1) Database User
+previleges: read, readWrite
+
+2) Database Administration
+previleges: read, readWrite, dbAdmin, userAdmin, dbOwner
+
+3) Cluster Administration
+previleges: read, readWrite, dbAdmin, userAdmin, dbOwner, clusterAdmin, clusterManager, clusterMonitor, host manager.
+
+4) Backup/Restore
+previleges: all previleges of cluster administration, backup, restore
+
+5) Super User
+previleges: all previleges
+
+In database level:
+
+1) Database User
+previleges: readAnyDatabase, readWriteAnyDatabase
+
+2) Database Administration: dbAdminAnyDatabase, userAdminAnyDatabase
+
+3) Super User:
+previleges: all
+
+Some security commands:
+Create security officer: Best practice
+db.createUser(
+  { user: "security_officer",
+    pwd: "h3ll0th3r3",
+    roles: [ { db: "admin", role: "userAdmin" } ]
+  }
+)
+
+Grant role to user:
+db.grantRolesToUser( "dba",  [ { db: "playground", role: "dbOwner"  } ] )
+
+Show role privileges:
+db.runCommand( { rolesInfo: { role: "dbOwner", db: "playground" }, showPrivileges: true} )
+
+
+
+<<<<<<<<<<<<<<<<<<<< Server Tools >>>>>>>>>>>>>>>>>>>>>>
+mongod -> mongod daemon
+mongos -> mongo shell (interactive shell)
+
+ -> List mongodb binaries:
+find /usr/bin/ -name "mongo*"
+
+-> Create new dbpath and launch mongod:
+mkdir -p ~/first_mongod
+mongod --port 30000 --dbpath ~/first_mongod --logpath ~/first_mongod/mongodb.log --fork  # we make new server fork from parent server
+
+
+-> Use mongostat to get stats on a running mongod process:
+mongostat --port 30000
+
+
+
+-> Use mongodump to get a BSON dump of a MongoDB collection:
+mongodump --port 30000 --db applicationData --collection products
+ls dump/applicationData/
+cat dump/applicationData/products.metadata.json
+
+-> Use mongorestore to restore a MongoDB collection from a BSON dump:
+mongorestore --drop --port 30000 dump/
+
+-> Use mongoexport to export a MongoDB collection to JSON or CSV (or stdout!):
+mongoexport --help
+mongoexport --port 30000 --db applicationData --collection products
+mongoexport --port 30000 --db applicationData --collection products -o products.json
+
+-> Tail the exported JSON file:
+tail products.json
+
+-> Use mongoimport to create a MongoDB collection from a JSON or CSV file:
+mongoimport --port 30000 products.json
+
 
 */
