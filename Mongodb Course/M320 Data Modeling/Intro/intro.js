7df1ec57ff5516f35e2e86fb0d9c2d5ce34e964a
Suraj Karki <suraj.karki500@gmail.com>
2020-08-18 17:45:39 +0545

    Finished intro

---
+++ Mongodb Course/M320 Data Modeling/Intro/intro.js
@@ -0,0 +1,14 @@
+/*
+<<<<<<<<<<<<<<<<<<<<<<<Data Modeling>>>>>>>>>>>>>>>>>>>>>>>
+Methodology:
+The methodology we use composed of three phases.
+-> The first phase is to describe the workload. In other words, it means gathering everything
+there is to know about how you would be using your data.
+
+-> The second phase is to identify the relationship between the different entities you need to 
+handle and choose how to model those relationship.
+
+-> The third phase is to apply design patterns or transformation to the current model.
+
+
+*/
